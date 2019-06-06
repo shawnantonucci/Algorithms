62755c369bc2bc1b3f1ae180faded6a4b4dc68cc
shawn <shawnantonucci2018@gmail.com>
2019-06-06 12:33:51 -0700

    got cookies to work without cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,10 +3,18 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, cache=[]):
+    if n == 0:
+      return 1
+    if n == 1:
+      return 1
+    if n == 2:
+      return 2
+
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
